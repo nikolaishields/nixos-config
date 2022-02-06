4348b2a0378a4a62e234234c474ae691913f2efa
Nikolai Shields <nikolai@nikolaishields.com>
2022-02-06 12:46:16 -0600

    Add base install configuration

---
+++ hosts/base/configuration.nix
@@ -0,0 +1,62 @@
+{ config, lib, pkgs, ... }:
+let
+  unstableTarball = fetchTarball
+    "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
+in {
+
+  imports = 
+  [
+    ../../modules/base-packages.nix
+    ../../modules/networking/networking.nix
+    ../../modules/users/nikolai.nix
+    ./hardware-configuration.nix
+  ];
+
+  boot = {
+    # TODO: ephemeral root
+    #  boot.initrd.postDeviceCommands = "zfs rollback -r rpool/local/root@blank";
+    supportedFilesystems = [ "ext4" "zfs" ];
+    kernelPackages = pkgs.linuxPackages_latest;
+    kernelModules = [ "kvm-intel" ];
+
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+
+    initrd = {
+      supportedFilesystems = [ "ext4" "zfs" ];
+      availableKernelModules = [ "xhci_pci" "nvme" ];
+    };
+  };
+
+  nixpkgs.config = {
+    allowUnfree = true;
+    autoOptimiseStore = true;
+    packageOverrides = pkgs: {
+      unstable = import unstableTarball { config = config.nixpkgs.config; };
+    };
+  };
+
+  time = {
+    timeZone = "America/Chicago";
+    hardwareClockInLocalTime = true;
+  };
+
+  hardware = {
+    cpu.intel.updateMicrocode = true;
+    enableAllFirmware = true;
+  };
+
+  networking = {
+    hostName = "base";
+  };
+
+  powerManagement = {
+    powertop.enable = true;
+  };
+
+  system.stateVersion = "21.11";
+
+}
+
