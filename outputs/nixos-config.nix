742573a779d621a975a8d3dc72a3822dd89575cf
Nikolai Shields <nikolai@nikolaishields.com>
2022-07-01 10:29:50 -0500

    remove erroneous config
    Signed-off-by: Nikolai Shields <nikolai@nikolaishields.com>

--- outputs/nixos-config.nix
+++ outputs/nixos-config.nix
@@ -1,21 +1,12 @@
 { lib, inputs, system, ... }:
 
 {
-  laptop = lib.nixosSystem {
+  framework = lib.nixosSystem {
     inherit system;
     specialArgs = { inherit inputs; };
     modules = [
       ../system/configuration.nix
       ../system/machine/framework
-    ];
-  };
-
-  base-server = lib.nixosSystem {
-    inherit system;
-    specialArgs = { inherit inputs; };
-    modules = [
-      ../system/machine/base
-      ../system/configuration.nix
     ];
   };
 }
